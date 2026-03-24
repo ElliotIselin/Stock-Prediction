0aa9a40bff29f6dc0467f599a8fbfe336946367c
ElliotIselin <Iselin.Elliot@gmail.com>
2026-03-24 02:19:36 -0400

    Update scipt.js

--- Stock Market Website/scipt.js
+++ Stock Market Website/scipt.js
@@ -1,14 +1,12 @@
-// Get references to the button and the text
 const button = document.getElementById('toggleButton');
 const text = document.getElementById('myText');
 
-// Add click event listener
 button.addEventListener('click', () => {
   if (text.style.display === 'none') {
-    text.style.display = 'block';      // Show text
-    button.textContent = 'Hide Text';   // Change button label
+    text.style.display = 'block';
+    button.textContent = 'Hide Basket';
   } else {
-    text.style.display = 'none';       // Hide text
-    button.textContent = 'Show Text';   // Change button label
+    text.style.display = 'none';
+    button.textContent = 'First Basket';
   }
 });
